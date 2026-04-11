063fcfb43dbab9d0741c2e75c783b2ba1ba571f2
orionb05 <orionbrown05@gmail.com>
2026-04-11 12:09:36 -0400

    Add first simple /proc module

--- hello-1.c
+++ hello-1.c
@@ -1,19 +1,58 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/kernel.h> 
+#include <linux/uaccess.h>
+#include <linux/version.h>
+#include <linux/proc_fs.h>
 
-static int __init hello_init(void){
-    pr_info("Hello World 1.\n");
+#define procfs_name "helloworld"
+
+static struct proc_dir_entry *our_proc_file;
+
+static ssize_t procfile_read(struct file *file_pointer, char __user *buffer,
+                             size_t buffer_length, loff_t *offset)
+{
+    char s[13] = "HelloWorld!\n";
+    int len = sizeof(s);
+
+    ssize_t ret = len;
+
+    if(*offset >= len || copy_to_user(buffer, s, len)){
+        pr_info("copy_to_user failed\n");
+        ret = 0;
+    } else {
+        pr_info("procfile read %s\n", file_pointer->f_path.dentry->d_name.name);
+        *offset += len;
+    }
+
+    return ret;
+}
+
+static const struct proc_ops proc_file_fops = {
+    .proc_read = procfile_read,
+};
+
+static int __init procfs1_init(void){
+    our_proc_file = proc_create(procfs_name, 0644, NULL, &proc_file_fops);
+
+    if (NULL == our_proc_file) {
+        pr_alert("Error: Could not initialize /proc/%s\n", procfs_name);
+        return -ENOMEM;
+    }
+
+    pr_info("/proc/%s created\n", procfs_name);
 
     // Nonzero return value means init_module failed to load
     return 0;
 }
 
-static void __exit hello_exit(void){
-    pr_info("Goodbye world 1.\n");
+static void __exit procfs1_exit(void){
+    proc_remove(our_proc_file);
+    pr_info("/proc/%s removed\n", procfs_name);
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(procfs1_init);
+module_exit(procfs1_exit);
 
 MODULE_LICENSE("GPL");
