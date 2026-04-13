08af28f4f6381a7e8be6dce983ec27aa24b0832c
orionb05 <orionbrown05@gmail.com>
2026-04-13 01:05:20 -0400

    Add kprobe registration and fix format

--- syscall_trace.c
+++ syscall_trace.c
@@ -6,7 +6,9 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 #include <linux/proc_fs.h>
+#include <linux/kprobes.h>
 #include <linux/sched.h>
+#include <linux/ktime.h>
 #include <linux/uaccess.h>
 #include <linux/version.h>
 #include <linux/seq_file.h>
@@ -22,14 +24,13 @@
 #define PROCFS_FILENAME "syscall-trace"
 
 #define INPUT_MAX_LEN 33
-static char input_buffer[INPUT_MAX_LEN];   
+static char input_buffer[INPUT_MAX_LEN];
 
-u64 run_until_time = 0;
+static u64 run_until_time = 0;
 
 static struct proc_dir_entry *entry;
 
 #define NUM_BUCKETS 64
-
 struct syscall_stats {
 	u64 count;
 	u64 total_latency;
@@ -37,19 +38,21 @@ struct syscall_stats {
 	u64 latency_hist[NUM_BUCKETS];
 };
 
-enum syscalls_tracked {
-	SYSCALL_READ,
-	SYSCALL_WRITE,
-	SYSCALL_GETPID,
-	NUM_SYSCALLS
-};
+enum syscalls_tracked { SYSCALL_READ, SYSCALL_MMAP, SYSCALL_FUTEX, SYSCALL_CLONE, NUM_SYSCALLS };
+
+static struct kprobe syscall_kprobes[NUM_SYSCALLS];
+static struct kprobe *syscall_kprobe_ptrs[NUM_SYSCALLS];
+
+static struct kretprobe syscall_kretprobes[NUM_SYSCALLS];
+static struct kretprobe *syscall_kretprobe_ptrs[NUM_SYSCALLS];
 
 static struct syscall_stats stats[NUM_SYSCALLS];
 
-static char *syscall_names[NUM_SYSCALLS] = {
-	[SYSCALL_READ] = "read",
-	[SYSCALL_WRITE] = "write",
-	[SYSCALL_GETPID] = "getpid",
+static char *syscall_symbols[NUM_SYSCALLS] = {
+	[SYSCALL_READ] = "__x64_sys_read",
+	[SYSCALL_MMAP] = "__x64_sys_mmap",
+	[SYSCALL_FUTEX] = "__x64_sys_futex",
+	[SYSCALL_CLONE] = "__x64_sys_clone",
 };
 
 static void *my_seq_start(struct seq_file *s, loff_t *pos)
@@ -66,7 +69,7 @@ static void *my_seq_start(struct seq_file *s, loff_t *pos)
 static void *my_seq_next(struct seq_file *s, void *v, loff_t *pos)
 {
 	(*pos)++;
-	if(*pos >= NUM_SYSCALLS){
+	if (*pos >= NUM_SYSCALLS) {
 		return NULL;
 	}
 
@@ -82,10 +85,10 @@ static int my_seq_show(struct seq_file *s, void *v)
 {
 	struct syscall_stats *stat = (struct syscall_stats *)v;
 
-	seq_printf(s, "%s:\n", syscall_names[stat - stats]);
-	seq_printf(s, "%Ld\n", stat->count);
-	seq_printf(s, "%Ld\n", stat->total_latency);
-	seq_printf(s, "%Ld\n", stat->max_latency);
+	seq_printf(s, "%s:\n", syscall_symbols[stat - stats]);
+	seq_printf(s, "%llu\n", stat->count);
+	seq_printf(s, "%llu\n", stat->total_latency);
+	seq_printf(s, "%llu\n", stat->max_latency);
 
 	return 0;
 }
@@ -100,20 +103,19 @@ static struct seq_operations my_seq_ops = {
 static int my_seq_open(struct inode *inode, struct file *file)
 {
 	return seq_open(file, &my_seq_ops);
-};
+}
 
-static ssize_t my_proc_write(struct file *file, const char __user *buff,
-                              size_t len, loff_t *off)
+static ssize_t my_proc_write(struct file *file, const char __user *buff, size_t len, loff_t *off)
 {
 	if (len == 0)
-    	return -EINVAL;
+		return -EINVAL;
 
-    if (len >= INPUT_MAX_LEN) {
+	if (len >= INPUT_MAX_LEN) {
 		pr_info("Input too long; max %d characters\n", INPUT_MAX_LEN - 1);
 		len = INPUT_MAX_LEN - 1;
 	}
 
-	if (copy_from_user(input_buffer, buff, len))  
+	if (copy_from_user(input_buffer, buff, len))
 		return -EFAULT;
 
 	input_buffer[len] = '\0'; // Null-terminate the input string
@@ -123,7 +125,7 @@ static ssize_t my_proc_write(struct file *file, const char __user *buff,
 	// Convert user input to an integer
 	unsigned int run_time_seconds;
 	if (kstrtoint(input_buffer, 10, &run_time_seconds))
-    	return -EINVAL;
+		return -EINVAL;
 
 	if (run_time_seconds == 0 || run_time_seconds > 1024)
 		return -EINVAL;
@@ -131,13 +133,12 @@ static ssize_t my_proc_write(struct file *file, const char __user *buff,
 	// TODO: Clean syscall stats
 
 	// Signal K-probe collection to run for the specified time
-	WRITE_ONCE(run_until_time,
-           ktime_get_ns() + run_time_seconds * NSEC_PER_SEC);
+	WRITE_ONCE(run_until_time, ktime_get_ns() + run_time_seconds * NSEC_PER_SEC);
 
 	pr_info("Starting collection at %llu seconds\n", ktime_get_ns() / NSEC_PER_SEC);
 	pr_info("Ending at %llu seconds\n", run_until_time / NSEC_PER_SEC);
-	
-    return len;
+
+	return len;
 }
 
 #ifdef HAVE_PROC_OPS
@@ -163,19 +164,47 @@ static const struct file_operations file_ops = {
 };
 #endif
 
+static int kprobe_callback(struct kprobe *kp, struct pt_regs *regs)
+{
+	// TODO: Identify the associated syscall and update the stats
+	return 0;
+}
+
+static int kretprobe_callback(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	// TODO: Identify the associated syscall and update the stats
+	return 0;
+}
+
 static int __init mod_init(void)
 {
 	entry = proc_create(PROCFS_FILENAME, 0, NULL, &file_ops);
 
 	if (entry == NULL) {
-		pr_info("Error: Could not initialize /proc/%s\n",
-			 PROCFS_FILENAME);
+		pr_info("Error: Could not initialize /proc/%s\n", PROCFS_FILENAME);
 		return -ENOMEM;
 	}
 
 	proc_set_user(entry, GLOBAL_ROOT_UID, GLOBAL_ROOT_GID);
 
 	pr_info("/proc/%s created\n", PROCFS_FILENAME);
+
+	// Attach probes to beginning and end of each syscall
+	for (int i = 0; i < NUM_SYSCALLS; i++) {
+		syscall_kprobes[i].symbol_name = syscall_symbols[i];
+		syscall_kretprobes[i].kp.symbol_name = syscall_symbols[i];
+
+		syscall_kprobes[i].pre_handler = &kprobe_callback;
+		syscall_kretprobes[i].handler = &kretprobe_callback;
+
+		// Store probe pointer for the registration functions
+		syscall_kprobe_ptrs[i] = &syscall_kprobes[i];
+		syscall_kretprobe_ptrs[i] = &syscall_kretprobes[i];
+	}
+
+	register_kprobes(syscall_kprobe_ptrs, NUM_SYSCALLS);
+	register_kretprobes(syscall_kretprobe_ptrs, NUM_SYSCALLS);
+
 	return 0;
 }
 
@@ -183,6 +212,9 @@ static void __exit mod_exit(void)
 {
 	remove_proc_entry(PROCFS_FILENAME, NULL);
 	pr_info("/proc/%s removed\n", PROCFS_FILENAME);
+
+	unregister_kprobes(syscall_kprobe_ptrs, NUM_SYSCALLS);
+	unregister_kretprobes(syscall_kretprobe_ptrs, NUM_SYSCALLS);
 }
 
 module_init(mod_init);
